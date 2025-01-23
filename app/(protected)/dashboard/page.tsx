545bc8a7d19cf7d1690671855e9751796b83df58
Abdulrahman Dauda <57319135+DoudGaya@users.noreply.github.com>
2025-01-23 12:10:40 +0100

    dialog

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+  } from "@/components/ui/dialog"
 
-const Dashboard = () => {
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
   return (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
+    <SidebarProvider>
+      <AppSidebar className=" bg-green-200" />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </div>
+        </header>
+       
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
-
-export default Dashboard
